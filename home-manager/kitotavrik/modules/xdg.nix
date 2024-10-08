3269e31ba924a5d49494b2035bc7ed4fea75f574
firefenix <kitotavrik.media@gmail.com>
2024-10-08 19:39:03 +0300

    Добавлены xdg директории.

---
+++ home-manager/kitotavrik/modules/xdg.nix
@@ -0,0 +1,20 @@
+_:
+
+{
+	xdg = {
+		enable = true;
+
+		userDirs = {
+			enable = true;
+
+			desktop = "$HOME/desktop";
+			download = "$HOME/downloads";
+			documents = "$HOME/documents";
+			templates = "$HOME/templates";
+			music = "$HOME/media/music";
+			videos = "$HOME/media/videos";
+			pictures = "$HOME/media/pictures";
+			publicShare = "$HOME/share/public";
+		};
+	};
+}
