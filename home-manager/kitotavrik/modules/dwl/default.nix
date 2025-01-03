277ac03cfbf9d1037629b47cbe6f6f01bf74548e
firefenix <kitotavrik.media@gmail.com>
2025-01-03 20:08:57 +0300

    Formating.

--- home-manager/kitotavrik/modules/dwl/default.nix
+++ home-manager/kitotavrik/modules/dwl/default.nix
@@ -1,12 +1,12 @@
 {
   pkgs,
-	config,
-	lib,
+  config,
+  lib,
   ...
 }:
 let
   dwl-custom = pkgs.dwl.overrideAttrs (_oldAttrs: {
-		# src = ./source;
+    # src = ./source;
     patches = [
       (pkgs.fetchpatch2 {
         url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/simpleborders/simpleborders-v0.7.patch";
@@ -27,30 +27,32 @@ let
     ];
   });
 
-	dwl-start = pkgs.writeScriptBin "dwl-start" ''
-			do=true
-			while $do ||  [ -f /tmp/restart_dwl ]; do
-			    do=false
-			    rm -rf /tmp/restart_dwl > /dev/null 2>&1
-			    dwl -s "swww-daemon" 2> ~/.cache/dwl.txt 
-			done
-		'';
+  dwl-start = pkgs.writeScriptBin "dwl-start" ''
+    			do=true
+    			while $do ||  [ -f /tmp/restart_dwl ]; do
+    			    do=false
+    			    rm -rf /tmp/restart_dwl > /dev/null 2>&1
+    			    dwl -s "swww-daemon" 2> ~/.cache/dwl.txt 
+    			done
+    		'';
 
 in
 {
   home.packages = with pkgs; [
     (dwl-custom.override {
-			configH = builtins.readFile (pkgs.substituteAll {
-				src = ./config.h;
-				root_color = config.lib.stylix.colors.base00;
-				border_color = config.lib.stylix.colors.base03;
-      	focus_color = config.lib.stylix.colors.base0D;
-				urgent_color = config.lib.stylix.colors.base08;
-				
-				edp1_scale = 1.74;
-			});
-		})
+      configH = builtins.readFile (
+        pkgs.substituteAll {
+          src = ./config.h;
+          root_color = config.lib.stylix.colors.base00;
+          border_color = config.lib.stylix.colors.base03;
+          focus_color = config.lib.stylix.colors.base0D;
+          urgent_color = config.lib.stylix.colors.base08;
 
-		dwl-start
+          edp1_scale = 1.74;
+        }
+      );
+    })
+
+    dwl-start
   ];
 }
